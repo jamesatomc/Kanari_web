4ba859090c25b0b4faaabeb1f7686d0a8e21d0ab
Jiraphon Wansai <jiraphonwansai@engineer.com>
2024-02-23 16:40:59 +0700

    edit ui 1

--- src/app/HeroSection.tsx
+++ src/app/HeroSection.tsx
@@ -6,12 +6,47 @@ interface Props {}
 const HeroSection: NextPage<Props> = ({}) => {
   return <div>
   <header>
-        <div className='container'>
-            <h1>Kanari Network</h1>
-            <h2>A Modern Bank Card For A Modern World</h2>
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="/iconkari.svg" className="h-8" alt="Kanari Network"/>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kanari Network</span>
+            </a>
+            <div className="items-center justify-items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                </li>
+                </ul>
+            </div>
         </div>
+    </nav>
     </header>
+
+
+
+
     <main>
+    <section className='features'>
+            <div className='container'>
+                <h2>We Tried To Provide You With All Global Banking Services</h2>
+                <ul>
+                    <li>Up-To-Date And Fast Banking Services In One Place</li>
+                    <li>Types Of Business And Personal Accounts</li>
+                    <li>Opening Of Essential Bank Account</li>
+                    <li>Information And Documents</li>
+                </ul>
+            </div>
+        </section>    
         <section className='features'>
             <div className='container'>
                 <h2>We Tried To Provide You With All Global Banking Services</h2>
@@ -23,14 +58,17 @@ const HeroSection: NextPage<Props> = ({}) => {
                 </ul>
             </div>
         </section>
-        <div className="container">
-          <div className="choices">
-            <button className=''>ตัวเลือก 1</button>
-            <button>ตัวเลือก 2</button>
-            <button>ตัวเลือก 3</button>
-            <button>ตัวเลือก 4</button>
-          </div>
-        </div>
+        <section className='features'>
+            <div className='container'>
+                <h2>We Tried To Provide You With All Global Banking Services</h2>
+                <ul>
+                    <li>Up-To-Date And Fast Banking Services In One Place</li>
+                    <li>Types Of Business And Personal Accounts</li>
+                    <li>Opening Of Essential Bank Account</li>
+                    <li>Information And Documents</li>
+                </ul>
+            </div>
+        </section>
     </main>
  
 
