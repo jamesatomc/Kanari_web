b69854bb6217fdf72460c6ae7d68e49ae12c5bc7
Jiraphon Wansai <jiraphonwansai@engineer.com>
2024-02-23 17:09:19 +0700

    edit ui -3.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SpeedInsights />
     </html>
   );
 }
